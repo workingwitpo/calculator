18b9c52022d2b56dcb50409ec01a27d7897a7c6b
Johnny <rackingacc@yahoo.com>
2023-01-28 18:14:39 -0600

    added logic

--- main.js
+++ main.js
@@ -16,16 +16,16 @@ function division(a,b){
 
 
 function operate(operator,num1,num2){
-    if (operator == 'addition'){
+    if (operator == 'add'){
         return addition(num1,num2);
     }
-    else if (operator =='subtraction'){
+    else if (operator =='subtract'){
         return subtraction(num1,num2);
     }
-    else if (operator =='multiplication'){
+    else if (operator =='multiply'){
         return multiplication(num1,num2);
     }
-    else if (operator =='division'){
+    else if (operator =='divide'){
         return division(num1,num2);
     }
 }
@@ -37,4 +37,28 @@ function clickButton(event){
 
 function clearButton(){
     let theDisplay = document.getElementById("display").innerHTML = "";
+}
+
+function delButton(){
+    let theDisplay = document.getElementById("display").innerHTML;
+    if (theDisplay != ""){
+        let delVal = theDisplay.slice(0,theDisplay.length-1)
+        document.getElementById("display").innerHTML = delVal;
+    
+    }
+}
+
+let currentNumber = 0;
+let nextNumber = 0;
+let operator; 
+function operateButton(event){
+    nextNumber = currentNumber;
+    operator = String(event.className);
+    currentNumber = parseInt(document.getElementById("display").innerHTML);
+    document.getElementById("display").innerHTML = "";
+}
+function equalsButton(){
+    nextNumber = parseInt(document.getElementById("display").innerHTML)
+    let answer = operate(operator,currentNumber,nextNumber);
+    document.getElementById("display").innerHTML = answer;
 }
